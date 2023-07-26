8c7295da4dc5572f202a0e6f7fc087bfb39f2f5b
venkatapechetty-01 <66048322+venkatapechetty-01@users.noreply.github.com>
2023-07-26 19:50:13 -0400

    models to single file

--- app/models/role.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Role(models.Model):
-    RoleID = models.AutoField(primary_key=True)
-    RoleName = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.RoleName
